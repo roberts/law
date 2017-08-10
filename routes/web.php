c025bba0a25fea173232400c4a4c81a7921ef6c3
DrewRoberts <drew@roberts.email>
2017-08-10 01:10:08 -0400

    Update Web Routes

--- routes/web.php
+++ routes/web.php
@@ -15,42 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('desk', function () {
-    return view('desk');
-});
-
-Route::get('conference', function () {
-    return view('conference');
-});
-
-Route::get('accounting', function () {
-    return view('accounting');
-});
-
-Route::get('files', function () {
-    return view('files.index');
-});
-
-Route::get('files/leads', function () {
-    return view('files.leads');
-});
-
-Route::get('files/pre', function () {
-    return view('files.pre');
-});
-
-Route::get('files/litigation', function () {
-    return view('files.litigation');
-});
-
-Route::get('files/closed', function () {
-    return view('files.closed');
-});
-
-Route::get('files/create', function () {
-    return view('files.create');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home');
+Route::get('desk', function () { return view('desk'); });
+Route::get('conference', function () { return view('conference'); });
+Route::get('accounting', function () { return view('accounting'); });
+Route::get('/@{user}', 'UsersController@show');
+Route::get('files', 'FilesController@index');
+Route::group(['prefix' => 'files'], function () {
+    Route::get('leads', 'FilesController@leads');
+    Route::get('pre', 'FilesController@pre');
+    Route::get('litigation', 'FilesController@litigation');
+    Route::get('closed', 'FilesController@closed');
+    Route::get('create', 'FilesController@create');
+    Route::get('{file}', 'FilesController@show');
+});
