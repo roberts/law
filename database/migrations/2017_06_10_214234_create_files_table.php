6b6fd31b80f372585b124d6ae807d0e0534bfc71
DrewRoberts <drew@roberts.email>
2017-08-11 22:12:37 -0400

    Create File Types

--- database/migrations/2017_06_10_214234_create_files_table.php
+++ database/migrations/2017_06_10_214234_create_files_table.php
@@ -18,11 +18,11 @@ class CreateFilesTable extends Migration
             $table->string('file_number')->index(); // YYYY-MM-XXXXX where YYYY-MM is year and month of creation and then auto-increase by 1 for the last 5 digits.
             $table->unsignedInteger('counsel'); // Firm that owns the matter, so it is organization id on contacts table. 
             // Can have multiple firms that have access to it as co-counsels via relations table
-            // Can have only one type as defined by fileable pivot table: Dust Mask, Car Wreck, DUI, Estate Planning, etc.
             // Can have multiple intake forms via fileables polymorphic pivot table
             // Can have more than one plaintiff through the intake forms
             // Can have more than one defendant through relations table
             // Current status determined by most recent entry on file_status table so has time stamp all of the changes
+            $table->unsignedInteger('file_type_id'); // Can have only one type: Dust Mask, Car Wreck, DUI, Estate Planning, etc.
             $table->unsignedInteger('source_id');
             $table->unsignedInteger('referral_id')->nullable(); // Use only when source_id is 1 or 2 (general referral or referral from law firm) & the referral contact is known
             $table->unsignedInteger('case_id')->nullable()->index(); // One case(litigation) may have have multiple matters(files)
---
+++ database/seeds/FileTypesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FileTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = date('Y-m-d H:i:s');
+        
+        DB::table('types')->insert([
+            'model'         => 'App\IntakeMask',
+            'title'         => 'Dust Mask',
+            'created_at'    => $now,
+            'updated_at'    => $now,
+        ]);
+    }
+}
